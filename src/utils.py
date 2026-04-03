23d918a761d97c2f5708295c9900a67ae89fae59
gustavocn121 <gustavo.cn121@gmail.com>
2026-04-02 22:20:41 -0300

    refactor: add logging to read_config

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import yaml
-
+import logging
 
 def read_config(config_path: str) -> dict:
+    logging.info(f'Reading config from {config_path}...')
     with open(config_path) as f:
         return yaml.safe_load(f)
