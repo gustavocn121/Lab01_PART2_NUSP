9c4743b1f7c489d823571a8c7bde4902329fb999
gustavocn121 <gustavo.cn121@gmail.com>
2026-04-03 15:30:58 -0300

    refactor: move db_connector to utils

--- src/load/db_connector.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-
-
-def get_db_connection(host: str, database: str, user: str, password: str, port: int):
-    conn = psycopg2.connect(host=host, database=database, user=user, password=password, port=port)
-    cursor = conn.cursor()
-    return cursor, conn
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,13 @@
 import logging
 
+import psycopg2
 import yaml
+
+
+def get_db_connection(host: str, database: str, user: str, password: str, port: int):
+    conn = psycopg2.connect(host=host, database=database, user=user, password=password, port=port)
+    cursor = conn.cursor()
+    return cursor, conn
 
 
 def read_config(config_path: str) -> dict:
